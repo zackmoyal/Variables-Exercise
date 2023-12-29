76b24b1581988b9a260e22f188c63a2ee6eb8d68
Zack Moyal <zack.moyal@gmail.com>
2023-12-28 19:27:54 -0800

    initial commit for Module 2 - C# Variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Declare and initialize variables
+            string name = "Zack";
+            int age = 29;
+            char middleInitial = 'D';
+            bool isOver18 = true;
+            double currentHouseTemp = 71;
+            decimal moneyInWallet = 100.00m;
+
+            //Interpolate these variables in a Console.WriteLine();
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"Current Temp: {currentHouseTemp}");
+            Console.WriteLine($"Money In Wallet: {moneyInWallet}");
         }
     }
 }
